121947851cc257d22f5e1975f01940e88cb6398b
Depanshu <122829642+Suya678@users.noreply.github.com>
2024-11-13 11:07:38 -0700

    Just some semantic fixes and alos added a started index.js

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#race-details-view").classList.toggle("hidden");
+    document.querySelector("#results-details-view").classList.toggle("hidden");
+
+});
